3cc76dc62404728a04a79cd2a78e267976529372
Ulunji Felix Mkandawire <designedbyulunji@gmail.com>
2024-06-16 12:32:21 +0200

    Complete Header Component for all screens

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,29 @@
 import "./header.scss";
 import UrbanAgePropLogo from "../../assets/images/urban-age-properties-temp-logo.svg";
 import { FaBars } from "react-icons/fa6";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 function Header() {
 	const navLinks = [
-		{ id: 1, linkName: "Home", linkUrl: "" },
-		{ id: 2, linkName: "Property", linkUrl: "" },
-		{ id: 3, linkName: "Pricing", linkUrl: "" },
-		{ id: 4, linkName: "Contact", linkUrl: "" },
-		{ id: 5, linkName: "About Us", linkUrl: "" },
+		{ id: 1, linkName: "Home", linkUrl: "#", active: true },
+		{ id: 2, linkName: "Property", linkUrl: "#", active: false },
+		{ id: 3, linkName: "Pricing", linkUrl: "#", active: false },
+		{ id: 4, linkName: "Contact", linkUrl: "#", active: false },
+		{ id: 5, linkName: "About Us", linkUrl: "#", active: false },
 	];
+
+	const [toggled, setToggleIcon] = useState(false);
+	const [input, setInput] = useState("");
+
+	function handleNavToggle() {
+		if (toggled === false) {
+			setToggleIcon(true);
+		} else {
+			setToggleIcon(false);
+		}
+	}
 
 	return (
 		<header className="urban-age-prop__header">
@@ -23,13 +37,21 @@ function Header() {
 			</div>
 			{/* urban-age-prop-nav_links */}
 			<nav className="urban-age-prop__nav-links">
-				<ul className="nav-links__ul">
+				<ul
+					className={
+						toggled ? "nav-links__ul active" : "nav-links__ul"
+					}
+				>
 					{navLinks.map((link) => {
 						return (
 							<li className="nav-links__li" key={link.id}>
 								<a
 									href={link.linkUrl}
-									className="nav-links__anchor"
+									className={
+										link.active
+											? "nav-links__anchor active"
+											: "nav-links__anchor"
+									}
 								>
 									{link.linkName}
 								</a>
@@ -38,13 +60,34 @@ function Header() {
 					})}
 				</ul>
 			</nav>
+			{/* urban-age-prop-search_tab */}
+			<span className="urban-age-prop-search_container">
+				<input
+					type="text"
+					value={input}
+					onChange={function (event) {
+						setInput(event.target.value);
+					}}
+					className="urban-age-prop-search_box"
+				/>
+				<FaSearch className="urban-age-prop-search_icon" />
+			</span>
 			{/* urban-age-prop-action_btn */}
 			<button className="urban-age-prop__action-btn btn">
 				Get in touch
 			</button>
 			{/* urban-age-prop-bars */}
-			<div className="urban-age-prop-nav_bars-container">
-				<FaBars className="urban-age-prop-nav_bars" />
+			<div
+				className="urban-age-prop-nav_bars-container"
+				onClick={handleNavToggle}
+			>
+				<span>
+					{toggled ? (
+						<IoClose className="urban-age-prop-nav_bars" />
+					) : (
+						<FaBars className="urban-age-prop-nav_bars" />
+					)}
+				</span>
 			</div>
 		</header>
 	);
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,7 @@
+import "./main.scss";
+
+function Main() {
+	return <main className="urban-age-prop__hero">Hello</main>;
+}
+
+export default Main;
